d796465dba63febe5fc9de6b6f3647125ec02cbf
francescopanico <frpanico96@gmail.com>
2023-11-14 23:16:23 +0100

    [JAVA] Data Structures

--- HelloWorld.java
+++ HelloWorld.java
@@ -8,6 +8,7 @@ public class HelloWorld {
     System.out.println("HelloWorld");
     typeConversion();
     conditions();
+    dataStructure();
   }
 
   static void typeConversion() {
@@ -50,6 +51,50 @@ public class HelloWorld {
       default:
         break;
     }
+  }
+
+  static void dataStructure() {
+    // Array declaration
+    // Suitable for application that require frequent searching
+    int size = 4;
+    Integer intArray[] = new Integer[size];
+    int[] intArray2 = new int[size];
+    // Scanner in = new Scanner(System.in);
+    // System.out.println("starting loop");
+    for (int i = 0; i < size; ++i) {
+      System.out.println("starting loop " + i);
+      intArray[i] = i;
+      intArray2[i] = i;
+    }
+    for (int i = 0; i < size; ++i) {
+      System.out.println(intArray[i] + "  " + intArray2[i]);
+    }
+    // System.out.println("end");
+
+    // LinkedList
+    // Suitable when memory is limited
+    // Suitable for application that require frequen insertion and deletion
+    LinkedList<String> cars = new LinkedList<String>();
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Ford");
+    cars.add("Mazda");
+    System.out.println(cars);
+
+    // Stack
+    // Linear Data Structure
+    // LIFO principle
+    // push(), pop(), isEmpty(), peek()
+    Stack<Integer> stackInt = new Stack<Integer>();
+    System.out.println("Stack is empty: " + stackInt.empty());
+    System.out.println("Pushing stack...");
+    stackInt.push(2);
+    stackInt.push(3);
+    System.out.println("Stack is empty: " + stackInt.empty());
+    System.out.println("Top Item: " + stackInt.peek());
+    System.out.println("Popping stack...");
+    stackInt.pop();
+    System.out.println("Top Item: " + stackInt.peek());
   }
 
 }
