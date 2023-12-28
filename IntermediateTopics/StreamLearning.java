7d702bfd81821c7a9004654736b3473aba853aeb
francescopanico <frpanico96@gmail.com>
2023-12-28 12:43:11 +0100

    [JAVA] Intermediate topics - Generics

---
+++ IntermediateTopics/StreamLearning.java
@@ -0,0 +1,9 @@
+package IntermediateTopics;
+
+import java.io.IOException;
+
+public class StreamLearning extends BasicExcerices {
+  @Override
+  public void exercise() throws IOException {
+  }
+}
